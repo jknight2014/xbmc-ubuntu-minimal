fbbbda9aef30b213de33e79b17d040b57413c95a
Matt Filetto <matt.filetto@gmail.com>
2014-03-08 10:34:05 -0800

    I think the dpkg race condition is actually coming from the removing of apparmor as it always gives you that teardown error so dpkg isnt quite done

--- 12.10/xbmc-ubuntuniversal-minimal-installer.sh
+++ 12.10/xbmc-ubuntuniversal-minimal-installer.sh
@@ -848,11 +848,14 @@ function selectAdditionalPackages()
 function optimizeInstallation()
 {
     showInfo "Optimizing installation..."
+
     sudo echo "none /tmp tmpfs defaults 0 0" >> /etc/fstab
+
     sudo service apparmor stop > /dev/null &2>1
     sudo service apparmor teardown > /dev/null &2>1
     sudo update-rc.d -f apparmor remove > /dev/null &2>1	
     sudo apt-get remove --purge apparmor -y > /dev/null &2>1
+    sleep 3
     
     createDirectory "$TEMP_DIRECTORY" 1 0
 	handleFileBackup $RSYSLOG_FILE 0 1
